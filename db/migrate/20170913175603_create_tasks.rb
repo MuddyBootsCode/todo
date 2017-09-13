79bb70afccdc08b577b60edc147a2c2cc24a9f3a
MuddyBootsCode <firsttogo98@gmail.com>
2017-09-13 18:14:06 +0000

    added respec and factory girl gems to application, created tests

--- db/migrate/20170913175603_create_tasks.rb
+++ db/migrate/20170913175603_create_tasks.rb
@@ -1,6 +1,7 @@
 class CreateTasks < ActiveRecord::Migration[5.0]
   def change
     create_table :tasks do |t|
+
         t.string :title
         t.boolean :done, default: false
       t.timestamps
